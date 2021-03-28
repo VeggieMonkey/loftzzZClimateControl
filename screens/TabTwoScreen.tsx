56262b3a0df7061f2f1ba186255abd94459c1621
Jonas Flunes <jonasflunes@Jonass-MacBook-Pro.local>
2021-03-28 21:28:15 +0200

    added button and slider

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,14 +1,49 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
+import { Alert, Button, StyleSheet } from 'react-native';
+import Slider from '@react-native-community/slider';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-export default function TabTwoScreen() {
+const OnOrOffButton = ({isFanOn}:{isFanOn:boolean}) => {
+  return (
+    <Button
+    title={isFanOn ? "On" : "Off"}
+    color="#f194ff"
+    //onPress={() => }
+  />
+  )
+}
+
+const TabTwoScreen = () => {
+  const [fanSpeed, setFanSpeed] = React.useState(50)
+const handleFanSpeedChange = (speed:number) => {
+  console.log('Fan speed', speed)
+  setFanSpeed(speed)
+}
+
+
+/* Fan onOr off?
+if fan speed = 0 return OFF
+
+
+
+*/
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Fan speed</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.fanText}>Fan control</Text>
+      <Slider
+        style={{width: 250, height: 40}}
+        minimumValue={0}
+        maximumValue={100}
+        step={10}
+        onSlidingComplete={handleFanSpeedChange}
+        minimumTrackTintColor="#FFFFFF"
+        maximumTrackTintColor="#FFFFFF"
+      />
+
+      <Text style={styles.fanText}>Fan speed: {fanSpeed}%</Text>
+      <OnOrOffButton isFanOn={fanSpeed === 0 ? false : true} />
     </View>
   );
 }
@@ -28,4 +63,9 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  fanText: {
+    fontSize: 40,
+    fontWeight: '200',
+  },
 });
+export default TabTwoScreen
